f1e9fb3ea37cbf1851e991c45d8ef2d263def9a1
Alistair Smith <alistairsmith01@gmail.com>
2021-12-07 07:15:03 +0000

    bruh

--- src/client/components/slash-command.tsx
+++ src/client/components/slash-command.tsx
@@ -5,10 +5,12 @@ export interface Props {
 
 export function SlashCommand(props: Props) {
 	return (
-		<div className="select-none shadow-lg bg-shark-900 px-3 py-2 rounded-md flex items-center text-white justify-between">
+		<div className="select-none shadow-lg bg-shark-900 dark:bg-shark-200 px-3 py-2 rounded-md flex items-center text-white dark:text-shark-900 justify-between">
 			<div>
 				<p className="font-bold">/{props.command}</p>
-				<p className="text-sm text-white text-opacity-75">{props.description}</p>
+				<p className="text-sm text-white dark:text-shark-900 text-opacity-75">
+					{props.description}
+				</p>
 			</div>
 
 			<div>
@@ -21,8 +23,8 @@ export function SlashCommand(props: Props) {
 export function SlashCommandStack(props: Props) {
 	return (
 		<div className="flex flex-col items-center">
-			<div className="w-[96%] bg-shark-900 bg-opacity-70 dark:bg-shark-200 dark:bg-opacity-50 h-2 rounded-t-md" />
-			<div className="w-[98%] bg-shark-900 bg-opacity-80 dark:bg-shark-200 h-2 rounded-t-md" />
+			<div className="w-[96%] bg-shark-900 bg-opacity-70 dark:bg-shark-200 dark:bg-opacity-25 h-2 rounded-t-md" />
+			<div className="w-[98%] bg-shark-900 bg-opacity-80 dark:bg-shark-200 dark:bg-opacity-50 h-2 rounded-t-md" />
 			<div className="w-full">
 				<SlashCommand {...props} />
 			</div>
