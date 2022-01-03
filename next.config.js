9e6e4788a26165e133d6632a87c5478da5b34b6f
Jack LaFond <6956351+jacc@users.noreply.github.com>
2022-01-03 12:09:26 -0500

    add /support

--- next.config.js
+++ next.config.js
@@ -7,6 +7,12 @@ module.exports = {
 					'https://discord.com/oauth2/authorize?scope=bot&client_id=822117936251928586&permissions=8',
 				permanent: true,
 			},
+			{
+				source: '/support',
+				destination:
+					'https://discord.gg/gdH3whZ9cj',
+				permanent: true,
+			},
 		];
 	},
 };
