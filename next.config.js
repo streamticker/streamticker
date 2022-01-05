c4a2c0472c9a863762fd745efb8cac29b2eb44b9
Alistair Smith <alistairsmith01@gmail.com>
2022-01-05 09:58:38 +0000

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -4,7 +4,7 @@ module.exports = {
 			{
 				source: '/invite',
 				destination:
-					'https://discord.com/api/oauth2/authorize?client_id=822117936251928586&permissions=8&scope=bot%20applications.commands',
+					'https://discord.com/api/oauth2/authorize?client_id=822117936251928586&permissions=1051664&scope=applications.commands%20bot',
 				permanent: true,
 			},
 			{
