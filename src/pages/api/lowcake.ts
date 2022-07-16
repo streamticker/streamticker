89754fbccc70550b34b0b7543e85c10b7b3ca214
Alistair Smith <hi@alistair.sh>
2022-07-16 11:20:29 -0400

    ok

---
+++ src/pages/api/lowcake.ts
@@ -0,0 +1,3 @@
+import {handler} from '../../server/scheduler';
+
+export default handler;
---
+++ src/server/scheduler.ts
@@ -0,0 +1,22 @@
+import axios from 'axios';
+import {api} from './api';
+
+interface TickerQueue {
+	//
+}
+
+export const handler = api({
+	async GET({req, res}) {
+		console.log('Scheduler request');
+	},
+
+	async POST({req}) {
+		console.log('Received data from lowcake:', req.body);
+	},
+});
+
+export async function enqueue() {
+	await axios.post('https://lowcake-api.otters.app/v1/queues', {
+		//
+	});
+}
