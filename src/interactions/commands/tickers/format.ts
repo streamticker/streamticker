4952b72fe0ee499a0ccc66bf30de2019c8753d20
Jack LaFond <hi@jack.link>
2022-07-16 22:14:02 -0400

    feat(format): add format command

--- src/interactions/commands/tickers/format.ts
+++ src/interactions/commands/tickers/format.ts
@@ -28,7 +28,7 @@ export class FormatCommand extends SlashCommand {
 					channel_types: [ChannelType.GUILD_VOICE],
 				},
 				{
-					name: 'input',
+					name: 'format',
 					description: `The channel to edit a format of. Use "${FORMATTER_REPLACER}" where you would like the value to appear.`,
 					required: false,
 					type: CommandOptionType.STRING,
@@ -38,66 +38,31 @@ export class FormatCommand extends SlashCommand {
 	}
 
 	async run(ctx: CommandContext) {
-		ctx.defer();
+		ctx.defer(true);
 
-		if (!ctx.guildID) {
-			throw new Error('This command can only be ran inside of a guild!');
+		if (!ctx.options.format.includes(FORMATTER_REPLACER)) {
+			throw new Error(`Format must contain ${FORMATTER_REPLACER} to place a value into.`);
 		}
 
-		if (!is(ctx.options.type, enumerate(TickerType))) {
-			throw new Error('Invalid ticker type!');
-		}
-
-		const existingOnChannel = await prisma.ticker.findFirst({
-			where: {channel_id: ctx.options.channel},
-		});
-
-		if (existingOnChannel) {
-			throw new Error('A ticker already exists on this channel!');
-		}
-
-		const harvester = harvesters[ctx.options.type];
-
-		let platformId: string | null = null;
-
-		if (harvester.validateInput) {
-			const result = await harvester.validateInput(ctx.options.input);
-
-			if (result.success) {
-				platformId = result.platform_id;
-			} else {
-				throw new Error(result.message);
-			}
-		}
-
-		const ticker = await prisma.ticker.create({
-			data: {
+		const found = await prisma.ticker.findFirst({
+			where: {
 				channel_id: ctx.options.channel,
-				guild_id: ctx.guildID,
-				type: ctx.options.type,
-				refresh_after: new Date(),
-				format: defaultTickerFormats[ctx.options.type], // TODO: get format from harvester
-				platform_id: platformId,
 			},
 		});
 
-		await harvester.harvest(ticker);
+		if (!found) {
+			throw new Error('This channel is not a valid ticker!');
+		}
 
-		ctx.send('Ticker created (add more data here)');
-
-		await logsnag.publish({
-			channel: 'tickers',
-			event: 'User created ticker',
-			icon: '🆕',
-			description: `${ctx.user.username}#${ctx.user.discriminator} created a ticker at ${
-				ctx.guildID
-			} ${ctx.options.input ? `with input ${ctx.options.input}` : ''}`,
-			tags: {
-				ticker: ctx.options.type,
-				channel: ctx.options.channel,
-				user: ctx.user.id,
+		await prisma.ticker.update({
+			where: {
+				channel_id: ctx.options.channel,
 			},
-			notify: true,
+			data: {
+				format: ctx.options.format,
+			},
 		});
+
+		ctx.send(`Format updated to ${ctx.options.format}.`);
 	}
 }
