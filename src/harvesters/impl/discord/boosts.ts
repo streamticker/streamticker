5b7dfc16c3c67b69565f9046f1d5dd6e1a7792bf
Alistair Smith <hi@alistair.sh>
2022-07-16 22:48:43 -0400

    e

--- src/harvesters/impl/discord/boosts.ts
+++ src/harvesters/impl/discord/boosts.ts
@@ -4,11 +4,7 @@ import {DiscordAPI} from './api';
 
 export const DISCORD_BOOSTS = createHarvester(TickerType.DISCORD_BOOSTS, {
 	requirement: TickerRequirement.VOTE,
-	async harvest(ticker, client, ctx) {
-		const guild = await client.guilds.fetch({
-			guild: ticker.guild_id,
-		});
-
-		return DiscordAPI.getBoosts(guild);
+	async harvest(ticker, utils) {
+		throw new Error('not implemented');
 	},
 });
