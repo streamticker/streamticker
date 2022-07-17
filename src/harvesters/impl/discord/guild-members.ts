2078b2e39fedf3a65b6bf386fb97edc043aa4f72
Alistair Smith <hi@alistair.sh>
2022-07-16 20:43:45 -0400

    lmfao

--- src/harvesters/impl/discord/guild-members.ts
+++ src/harvesters/impl/discord/guild-members.ts
@@ -5,8 +5,6 @@ import {DiscordAPI} from './api';
 export const DISCORD_MEMBERS = createHarvester(TickerType.DISCORD_MEMBERS, {
 	requirement: TickerRequirement.NONE,
 	async harvest(ticker, utils: HarvesterUtils) {
-		utils.ensureId(ticker);
-
 		return DiscordAPI.getGuildMemberCount(ticker.guild_id);
 	},
 });
