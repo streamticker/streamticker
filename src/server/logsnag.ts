ce98c10c80fb8feb45988d952017be5113b981b3
Alistair Smith <hi@alistair.sh>
2022-07-17 00:43:30 -0400

    hack

--- src/server/logsnag.ts
+++ src/server/logsnag.ts
@@ -1,6 +1,6 @@
 import {LogSnag} from 'logsnag';
 
 export const logsnag = new LogSnag({
-	token: '9c45e1f436bf9f472f311fc6a3c76702',
+	token: '3d935f8885ac94aaf8a955b6659dc2ec',
 	project: 'streamticker',
 });
