22340764e37b6fa20839e6f3a2a338614e175884
Alistair Smith <hi@alistair.sh>
2022-07-16 20:53:08 -0400

    0k

--- src/harvesters/impl/discord/guild-members.ts
+++ src/harvesters/impl/discord/guild-members.ts
@@ -1,10 +1,11 @@
 import {TickerType} from '@prisma/client';
-import {createHarvester, HarvesterUtils, TickerRequirement} from '../../harvester';
+import {createHarvester, TickerRequirement} from '../../harvester';
 import {DiscordAPI} from './api';
 
 export const DISCORD_MEMBERS = createHarvester(TickerType.DISCORD_MEMBERS, {
 	requirement: TickerRequirement.NONE,
-	async harvest(ticker, utils: HarvesterUtils) {
-		return DiscordAPI.getGuildMemberCount(ticker.guild_id);
+	async harvest(ticker) {
+		const members = await DiscordAPI.getGuildMembers(ticker.guild_id);
+		return members.length;
 	},
 });
