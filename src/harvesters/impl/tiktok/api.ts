308d919ad8b3cfb8ed42aaa65b5a165b4a430387
Jack LaFond <hi@jack.link>
2022-07-20 11:56:47 -0400

    LMFAOOOO I FORGOT A KEYWORD

--- src/harvesters/impl/tiktok/api.ts
+++ src/harvesters/impl/tiktok/api.ts
@@ -3,13 +3,13 @@ import {TikTokUser} from './types';
 
 export const TikTokAPI = {
 	async getUser(username: string) {
-		const {data} = await axios.get<TikTokUser>(`https://wadokei.hop.sh/${username}`);
+		const {data} = await axios.get<TikTokUser>(`https://wadokei.hop.sh/tiktok/${username}`);
 
 		return data.userInfo;
 	},
 
 	async getUserFollows(username: string) {
-		const {data} = await axios.get<TikTokUser>(`https://wadokei.hop.sh/${username}`);
+		const {data} = await axios.get<TikTokUser>(`https://wadokei.hop.sh/tiktok/${username}`);
 
 		return data.userInfo.stats.followerCount;
 	},
