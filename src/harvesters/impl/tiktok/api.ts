ff76a633369e1e4210a83b95738a4f703d3ce823
Jack LaFond <hi@jack.link>
2022-07-20 12:05:58 -0400

    forgot auth on wadokei woops

--- src/harvesters/impl/tiktok/api.ts
+++ src/harvesters/impl/tiktok/api.ts
@@ -1,15 +1,20 @@
 import axios from 'axios';
+import {env} from '../../../server/env';
 import {TikTokUser} from './types';
 
 export const TikTokAPI = {
 	async getUser(username: string) {
-		const {data} = await axios.get<TikTokUser>(`https://wadokei.hop.sh/tiktok/${username}`);
+		const {data} = await axios.get<TikTokUser>(`https://wadokei.hop.sh/tiktok/${username}`, {
+			headers: {Authorization: env.WADOKEI_KEY},
+		});
 
 		return data.userInfo;
 	},
 
 	async getUserFollows(username: string) {
-		const {data} = await axios.get<TikTokUser>(`https://wadokei.hop.sh/tiktok/${username}`);
+		const {data} = await axios.get<TikTokUser>(`https://wadokei.hop.sh/tiktok/${username}`, {
+			headers: {Authorization: env.WADOKEI_KEY},
+		});
 
 		return data.userInfo.stats.followerCount;
 	},
