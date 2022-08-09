c6c32f5078a2d8097bb3b1f9878a3925240b89ae
Alistair Smith <hi@alistair.sh>
2022-08-09 14:18:04 +0100

    you have to be FUCKING kidding me

--- ci/pull-env.mjs
+++ ci/pull-env.mjs
@@ -1,11 +1,7 @@
 import {Hop} from '@onehop/js';
 import {writeFile} from 'fs/promises';
 
-// Securely-ish print out the token because i cant tell if
-// darn github actions is setting it or not...
-console.log(process.env.HOP_PROJECT_TOKEN?.substring(0, 10));
-
-const hop = new Hop(process.env.HOP_PROJECT_TOKEN);
+const hop = new Hop(process.env.HOP_API_KEY);
 
 const deployment = await hop.ignite.deployments.get('deployment_bru');
 
