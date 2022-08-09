8864bd9fcac37a0a3c70a3c746fa40ac0d8407e2
Alistair Smith <hi@alistair.sh>
2022-08-09 14:22:37 +0100

    deployment_MzMyMzc0ODA5MDAyNTU3NzQ

--- ci/pull-env.mjs
+++ ci/pull-env.mjs
@@ -3,7 +3,7 @@ import {writeFile} from 'fs/promises';
 
 const hop = new Hop(process.env.HOP_API_KEY);
 
-const deployment = await hop.ignite.deployments.get('deployment_bru');
+const deployment = await hop.ignite.deployments.get(process.env.HOP_DEPLOYMENT_ID);
 
 const stringifiedEnv = Object.entries(deployment.config.env)
 	.map(entry => {
