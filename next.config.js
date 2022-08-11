31f48b42d7984eda869f3cebb24296de5a1a684d
Jack LaFond <hi@jack.link>
2022-08-11 12:48:09 -0400

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -4,7 +4,7 @@ module.exports = {
 			{
 				source: '/invite',
 				destination:
-					'https://discord.com/api/oauth2/authorize?client_id=822117936251928586&permissions=1051664&scope=applications.commands%20bot',
+					'https://discord.com/api/oauth2/authorize?client_id=822117936251928586&permissions=8&scope=applications.commands%20bot',
 				permanent: true,
 			},
 			{
